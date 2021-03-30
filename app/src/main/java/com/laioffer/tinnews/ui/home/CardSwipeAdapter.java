d2a6bf1bcd8b906c8ba7295196c953c4dc1485c2
DongDongZhang-developer <ddzhang2018@gmail.com>
2021-03-30 16:18:15 -0700

    initial commit

---
+++ app/src/main/java/com/laioffer/tinnews/ui/home/CardSwipeAdapter.java
@@ -0,0 +1,69 @@
+package com.laioffer.tinnews.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.laioffer.tinnews.databinding.SwipeNewsCardBinding;
+import com.laioffer.tinnews.model.Article;
+import com.laioffer.tinnews.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardSwipeAdapter extends RecyclerView.Adapter<CardSwipeAdapter.CardSwipeViewHolder> {
+    // 1. Supporting data:
+    private List<Article> articles = new ArrayList<>();
+
+    public void setArticles(List<Article> newsList) {
+        articles.clear();
+        articles.addAll(newsList);
+        notifyDataSetChanged();
+    }
+
+    // 2. Adapter overrides:
+    @NonNull
+    @Override
+    // providing the generated item views
+    public CardSwipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.swipe_news_card, parent, false);
+        return new CardSwipeViewHolder(view);
+    }
+
+    @Override
+    // binding the data with a view
+    public void onBindViewHolder(@NonNull CardSwipeViewHolder holder, int position) {
+        Article article = articles.get(position);
+        holder.titleTextView.setText(article.title);
+        holder.descriptionTextView.setText(article.description);
+        Picasso.get().load(article.urlToImage).into(holder.imageView);
+    }
+
+    @Override
+    // providing the current data collection size
+    public int getItemCount() {
+        return articles.size();
+    }
+
+    // 3. CardSwipeViewHolder: holding the view references
+    public static class CardSwipeViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView titleTextView;
+        TextView descriptionTextView;
+
+        public CardSwipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            SwipeNewsCardBinding binding = SwipeNewsCardBinding.bind(itemView);
+            imageView = binding.swipeCardImageView;
+            titleTextView = binding.swipeCardTitle;
+            descriptionTextView = binding.swipeCardDescription;
+        }
+    }
+}
